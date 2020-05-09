841673b0d9427c5c9c84bbfb88d2fe3e9b62d2a5
ABBAS MHD SALIM <57336556+PRINCEXDM@users.noreply.github.com>
2020-05-09 15:47:48 +0000

    Update colorprofilepic.py

--- stdplugins/colorprofilepic.py
+++ stdplugins/colorprofilepic.py
@@ -57,7 +57,7 @@ async def autopic(event):
     
         shutil.copy(downloaded_file_name, photo)
         im = Image.open(photo)
-        current_time = datetime.now().strftime("\n \n \n \n \n \n \n Owner: @kirito6969 . It's Her Choice (- Lawdi) \n \n \n \n \n \n \n \n \n                   Nikal Lawde \n                   Time: %H:%M:%S \n                   Date: %d/%m/%y ")
+        current_time = datetime.now().strftime("\n \n \n \n \n \n \n Owner: @꧁𓆩✰ചക്രവർത്തി✰𓆪꧂ . \n \n \n \n \n \n \n \n \n                   Time: %H:%M:%S \n                   Date: %d/%m/%y ")
         img = Image.open(photo)
         drawn_text = ImageDraw.Draw(img)
         fnt = ImageFont.truetype(FONT_FILE_TO_USE, 35)
